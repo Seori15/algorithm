cfb74164ccdb4a8ed7e3c8c5e96b4c270b5ae615
wlstn6278 <wlstn6278@gmail.com>
2022-09-26 19:00:55 +0900

    SWEA 보충

--- SWEA/SWEA_5789_현주의 상자 바꾸기.py
+++ SWEA/SWEA_5789_현주의 상자 바꾸기.py
@@ -13,3 +13,21 @@ for test_case in range(1, T+1):
 
     arr = map(str, arr)
     print(f'#{test_case} {" ".join(arr)}')
+
+
+# 보충수업
+# 입력값 설정
+T = int(input())
+for test_case in range(1, T+1):
+    N, Q = map(int, input().split())
+
+    # 길이 N만큼의 boxes 리스트 생성 후 입력 받을 때마다 L-R 범위의 숫자를 바꿔줌
+    boxes = [0]*(N+1)
+    for i in range(1, Q+1):
+        L, R = map(int, input().split())
+        for j in range(L, R+1):
+            boxes[j] = i
+    boxes.pop(0)
+
+    # 출력
+    print(f'#{test_case}', *boxes)
--- SWEA/SWEA_6485_삼성시의 버스 노선.py
+++ SWEA/SWEA_6485_삼성시의 버스 노선.py
@@ -17,3 +17,25 @@ for test_case in range(1, T+1):
     for c in bus_stop:
         print(bus_visit[c-1], end = ' ')
     print()
+
+# 보충수업
+# 입력값 설정
+T = int(input())
+for test_case in range(1, T+1):
+    N = int(input())
+    buses = [tuple(map(int, input().split())) for _ in '_'*N]
+    stop = [0]*(5001)
+    P = int(input())
+    lst_C = [int(input()) for _ in '_'*P]
+
+
+    # 입력받은 버스 노선에 따라 각 정류장을 지나는 버스 수 +1
+    for (A, B) in buses:
+        for i in range(A, B+1):
+            stop[i] += 1
+
+    # 출력 설정
+    print(f'#{test_case}', end=' ')
+    for i in lst_C:
+        print(stop[i], end=' ')
+    print()
