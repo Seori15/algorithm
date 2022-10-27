a49bd8cfcad0b35b51a9666b149586b37234b039
wlstn6278 <wlstn6278@gmail.com>
2022-10-27 23:35:34 +0900

    BOJ 문제풀이

--- python/BOJ/BOJ_1405_미친 로봇.py
+++ python/BOJ/BOJ_1405_미친 로봇.py
@@ -5,7 +5,7 @@ dj = [1, -1, 0, 0] # 동서남북
 # [B] DFS 함수 설정
 def dfs(i, j, cnt, pc):
     global result
-    # 가지치기 조건.
+    # 가지치기 조건. 확률이 0이라면 반환
     if pc == 0:
         return
 
@@ -17,7 +17,7 @@ def dfs(i, j, cnt, pc):
     # 범위에 맞게 dfs 탐색
     for dr in range(4):
         ni, nj = i+di[dr], j+dj[dr]
-        if -n <= ni <= n and -n <= nj <= n and not visited[ni][nj]:
+        if 0 <= ni <= 2*n and 0 <= nj <= 2*n and not visited[ni][nj]:
             visited[ni][nj] = 1
             dfs(ni, nj, cnt+1, pc*percent[dr])
             visited[ni][nj] = 0
@@ -27,8 +27,8 @@ n, E, W, S, N = map(int, input().split())
 percent = [E/100, W/100, S/100, N/100]
 
 visited = [[0]*(2*n+1) for _ in '_'*(2*n+1)]
-visited[0][0] = 1
+visited[n][n] = 1 # 시작점을 0,0이 아닌 n,n으로 잡는 것이 포인트
 result = 0
-dfs(0, 0, 0, 1)
+dfs(n, n, 0, 1)
 
 print(result)
